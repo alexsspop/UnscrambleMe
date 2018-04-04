db699e0b18abf4196cd243e9ac2bc124c9113f8f
Michael Douglas Pacheco <mdpgd95@gmail.com>
2018-04-04 09:01:55 -0300

    Add comments

--- src/server/UserThread.java
+++ src/server/UserThread.java
@@ -3,9 +3,16 @@ package server;
 import java.io.*;
 import java.net.*;
 
+/**
+ * This class is responsible to receive messages from a single client and redirect it to the server.
+ * Each connected client have a instance of UserThread running in server side.
+ */
 public class UserThread extends Thread {
+    // The client socket reference. Used to communicate with the client
     private Socket userSocket;
+    // A server reference used to communicate with the server
     private UnscrambleMeServer server;
+    // A reference used to send messages to the client
     private PrintWriter userWriter;
 
     public UserThread(Socket socket, UnscrambleMeServer server) {
@@ -15,12 +22,15 @@ public class UserThread extends Thread {
 
     public void run() {
         try {
+            // Retrieving the reference used to send messages to the client
             OutputStream output = userSocket.getOutputStream();
             userWriter = new PrintWriter(output, true);
 
+            // Retrieving the reference used to read messages sent by the client
             InputStream input = userSocket.getInputStream();
             BufferedReader userReader = new BufferedReader(new InputStreamReader(input));
 
+            // Waiting for client messages
             String userName = userReader.readLine();
             server.addUserName(userName);
             printUsers();
@@ -32,10 +42,12 @@ public class UserThread extends Thread {
 
             while (true) {
                 // TODO parse client input options
+                // Waiting for client message
                 clientMessage = userReader.readLine();
                 if (clientMessage == null)
                     break;
                 serverMessage = "[" + userName + "]: " + clientMessage;
+                // Broadcasting the received message
                 server.broadcast(serverMessage, this);
 
             }
@@ -47,6 +59,7 @@ public class UserThread extends Thread {
             server.broadcast(serverMessage, this);
 
         } catch (IOException ex) {
+            // Failed to retrieve the user message
             System.out.println("Error in UserThread: " + ex.getMessage());
             ex.printStackTrace();
         }
